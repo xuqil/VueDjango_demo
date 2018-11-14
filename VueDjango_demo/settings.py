890778baca664c6baaa5784c8ecbb69188c9eb14
许其龙 <13829842679@163.com>
2018-11-14 22:39:12 +0800

    模板路径

--- VueDjango_demo/settings.py
+++ VueDjango_demo/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  # 项目所在路径
 
 
 # Quick-start development settings - unsuitable for production
@@ -54,10 +54,10 @@ ROOT_URLCONF = 'VueDjango_demo.urls'
 
 TEMPLATES = [
     {
+        # 模板查找具有优先顺序
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
-        ,
-        'APP_DIRS': True,
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],  # 模板文件路径
+        'APP_DIRS': True,  # 在app下寻找模板文件
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
