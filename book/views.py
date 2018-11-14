ee9989e894fe60decb52ac82af8d8bab46693e48
许其龙 <13829842679@163.com>
2018-11-14 22:03:07 +0800

    视图参数

--- book/views.py
+++ book/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def index(request):
+    return HttpResponse("你好")
+
+
+def book(request, book_id,book_idd):
+    text = "你的书ID:" + book_id + book_idd
+    return HttpResponse(text)
