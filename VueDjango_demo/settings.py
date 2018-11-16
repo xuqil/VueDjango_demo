3482ee9e9f728d88e24c725890cad427227d0226
许其龙 <13829842679@163.com>
2018-11-16 21:57:28 +0800

    静态文件

--- VueDjango_demo/settings.py
+++ VueDjango_demo/settings.py
@@ -120,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+# 与APP无挂钩的静态文件
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
