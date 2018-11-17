d85f6efe56e8d491c0534060bfc7e0f654417d03
许其龙 <13829842679@163.com>
2018-11-17 13:59:48 +0800

    数据库模型映射

--- book/models.py
+++ book/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100, null=False)
+    author = models.CharField(max_length=100, null=False)
+    price = models.FloatField(null=False, default=0)
