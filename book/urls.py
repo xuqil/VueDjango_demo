abc4a874fc70b343abb0a808886c8f9272a0ac82
xuqil <13829842679@163.com>
2018-11-25 00:41:25 +0800

    表单1

--- book/urls.py
+++ book/urls.py
@@ -4,7 +4,8 @@ from book import views
 app_name = 'book'
 
 urlpatterns = [
-    path('', views.index),
+    # path('', views.index),
+    path('', views.IndexView.as_view()),
     path('detail<book_id>/<book_idd>', views.book),
     path('book/author', views.author_detail),
 ]
--- book/views.py
+++ book/views.py
@@ -2,7 +2,31 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template.loader import render_to_string
 from .models import Book
+from .forms import MessageBoardForm
+from django.views.generic import View
 
+
+class IndexView(View):
+    def get(self, request):
+        form = MessageBoardForm()
+        return render(request, 'form.html', context={'form': form})
+
+    def post(self, request):
+        form = MessageBoardForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data.get('title')  # cleaned_data代表已经验证过清理过的数据
+            content = form.cleaned_data.get('content')
+            email = form.cleaned_data.get('email')
+            reply = form.cleaned_data.get('reply')
+            print('='*30)
+            print(title)
+            print(content)
+            print(email)
+            print(reply)
+            return HttpResponse("ok")
+        else:
+            print(form.errors)
+            return HttpResponse("error")
 
 class Person(object):
     def __init__(self, username):
