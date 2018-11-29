ff0bc600a90adfa17cad6f967265a1a64a431a54
xuqil <13829842679@163.com>
2018-11-29 18:09:15 +0800

    使用sqlalchemy查询

--- sqlcheck/models.py
+++ sqlcheck/models.py
@@ -1,3 +1,29 @@
-from django.db import models
+from sqlalchemy import Column, DateTime, Integer, String, Float
+from sqlalchemy.ext.declarative import declarative_base
 
-# Create your models here.
+Base = declarative_base()
+metadata = Base.metadata
+
+
+class Students(Base):
+    __tablename__ = 'Students'
+    name = Column(String(100), nullable=False)
+    gender = Column(Integer, nullable=False)
+
+
+class Courses(Base):
+    __tablename__ = 'Courses'
+    name = Column(String(100), nullable=False)
+    teacher_id = Column(Integer, nullable=False)
+
+
+class Scores(Base):
+    __tablename__ = 'Scores'
+    student_id = Column(Integer, nullable=False)
+    course_id = Column(Integer, nullable=False)
+    number = Column(Float, nullable=False)
+
+
+class Teachers(Base):
+    __tablename__ = 'Teachers'
+    name = Column(String(100), nullable=False)
--- sqlcheck/views.py
+++ sqlcheck/views.py
@@ -1,3 +1,76 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
-# Create your views here.
+
+def check_avg_score(request):
+    pass
+    return HttpResponse("OK")
+
+
+def check_students_detail(request):
+    pass
+    return HttpResponse("OK")
+
+
+def check_teachers_number(request):
+    pass
+    return HttpResponse("OK")
+
+
+def check_student_no_done(request):
+    pass
+    return HttpResponse("ok")
+
+
+def check_student_done(request):
+    pass
+    return HttpResponse("OK")
+
+
+def check_student_huang(request):
+    pass
+    return HttpResponse("ok")
+
+
+def check_student_score_last(request):
+    pass
+    return HttpResponse("OK")
+
+
+def check_student_all_no_done(request):
+    pass
+    return HttpResponse("OK")
+
+
+def check_score_avg(request):
+    pass
+    return HttpResponse("OK")
+
+
+def check_all_scores(request):
+    pass
+    return HttpResponse("OK")
+
+
+def check_course_avg(request):
+    pass
+    return HttpResponse("Ok")
+
+
+def check_sex_total(request):
+    pass
+    return HttpResponse("Ok")
+
+
+def add_score(request):
+    pass
+    return HttpResponse('ok')
+
+
+def check_fail_students(request):
+    pass
+    return HttpResponse("OK")
+
+
+def check_number(request):
+    pass
+    return HttpResponse("ok")
